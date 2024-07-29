0f4fff921e94990fd9f71f69de36e12e1fd592be
axrorbekdf <58927436+axrorbekdf@users.noreply.github.com>
2024-07-29 08:01:15 +0000

    Deploying to gh-pages from @ axrorbekdf/uzman-admin-front@3c5ae08c27795060844b81d4952b12c82e68d504 🚀

---
+++ _nuxt/CajaBaWv.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c,a as t}from"./CpEHuMSc.js";const n={},s=t("h1",null,"Home",-1),_=[s];function a(r,d){return c(),o("div",null,_)}const i=e(n,[["render",a]]);export{i as default};

ec3b82b9e8d3eb7a07fd24bffd1d09f70de03a2d
axrorbekdf <58927436+axrorbekdf@users.noreply.github.com>
2024-08-02 13:09:57 +0000

    Deploying to gh-pages from @ axrorbekdf/uzman-admin@a677b110835da970e6fd07b2e3e05a6c375b39aa 🚀

--- _nuxt/CmQ_MGmO.js
+++
@@ -1,1 +0,0 @@
-import{ai as r,aj as e}from"./BwuoVgSa.js";import{A as a}from"./CyO0X899.js";import"./CkKw5vfm.js";const c=r(async(o,i)=>{a.getUser().then(t=>{if(!t.status)return console.log("AUTH run"),e("/auth/login")}).catch(t=>e("/auth/login"))});export{c as default};

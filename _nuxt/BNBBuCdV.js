4106bcd400d9ec5d056b79b6ec7828c3bf75df48
axrorbekdf <58927436+axrorbekdf@users.noreply.github.com>
2024-08-08 10:56:56 +0000

    Deploying to gh-pages from @ axrorbekdf/uzman-admin@3749edaff354c9519ee330f7996b3deabe1bb9b5 🚀

---
+++ _nuxt/BNBBuCdV.js
@@ -0,0 +1,1 @@
+import{af as r,ag as e}from"./CZtNgAxi.js";import{A as a}from"./CyO0X899.js";import"./CkKw5vfm.js";const f=r(async(o,n)=>{a.getUser().then(t=>{if(!t.status)return console.log("AUTH run"),e("/auth/login")}).catch(t=>e("/auth/login"))});export{f as default};
